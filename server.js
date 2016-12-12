b04b01842a86ed04bc1f2e8c3039ec5adaf1b64c
dajonline <daj@dajonline.com>
2016-12-12 01:40:14 +0000

    Fix for ipv4 output

--- server.js
+++ server.js
@@ -2,18 +2,16 @@ var PORT = process.env.PORT || 3000;
 
 var express = require('express');
 
-var morgan = require('morgan');
-
 var API = "/api/whoami";
 
 var app = express();
 
-app.use(morgan('dev'));
 
 app.get(API, function(request, response){
 	var language = request.headers['accept-language'];
 	var userAgent = request.headers['user-agent'];
-	var ip = request.ip
+//	var ip = request.ip
+var ip = request.headers["x-forwarded-for"] || request.connection.remoteAddress;
 	console.log(request);
 	response.json({
 		language: language,
